73448a9b648a3a2dbd038486bff7c3512b60bc9c
Matej Rábek <matej.rabek@gmail.com>
2021-05-05 09:44:22 +0200

    initial commit

--- web/public/index.php
+++ web/public/index.php
@@ -1,15 +1,43 @@
 <?php
 
-include '../app/vendor/autoload.php';
-$foo = new App\Acme\Foo();
+use App\Controller\PersonController;
 
-?><!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Docker <?php echo $foo->getName(); ?></title>
-    </head>
-    <body>
-        <h1>Docker <?php echo $foo->getName(); ?></h1>
-    </body>
+include '../app/vendor/autoload.php';
+
+$personController = new PersonController();
+$people = $personController->getAllPeople();
+?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>The HTML5 Herald</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+    <script src="./js/script.js"></script>
+
+  <style>
+      table, tr, td {
+          border: 1px solid black;
+          border-collapse: collapse;
+      }
+  </style>
+
+</head>
+
+<body>
+    <h1>OH</h1>
+
+    <ul>
+        <?php
+            foreach ($people as $person) {
+                echo "<li>".$person->getFullName()." <a href='editPerson.php?id=".$person->getId()."'>edit</a></li>";
+            }
+        ?>
+    </ul>
+
+</body>
 </html>
