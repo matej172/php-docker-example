ae07529a962e4a419224d818ad73eaff50026f33
matej172 <matej.rabek@gmail.com>
2022-05-06 23:39:53 +0200

    ADD: octave

--- web/public/index.php
+++ web/public/index.php
@@ -4,8 +4,6 @@ use App\Controller\PersonController;
 
 include '../app/vendor/autoload.php';
 
-$personController = new PersonController();
-$people = $personController->getAllPeople();
 ?>
 
 <!doctype html>
@@ -29,15 +27,12 @@ $people = $personController->getAllPeople();
 </head>
 
 <body>
-    <h1>OH</h1>
+    <h1>final zadanie</h1>
 
-    <ul>
-        <?php
-            foreach ($people as $person) {
-                echo "<li>".$person->getFullName()." <a href='editPerson.php?id=".$person->getId()."'>edit</a></li>";
-            }
-        ?>
-    </ul>
-
+    <?php 
+      $output = "";
+      exec('octave-cli --eval "m1 = 2500; m2 = 320;k1 = 80000; k2 = 500000;b1 = 350; b2 = 15020;pkg load control;A=[0 1 0 0;-(b1*b2)/(m1*m2) 0 ((b1/m1)*((b1/m1)+(b1/m2)+(b2/m2)))-(k1/m1) -(b1/m1);b2/m2 0 -((b1/m1)+(b1/m2)+(b2/m2)) 1;k2/m2 0 -((k1/m1)+(k1/m2)+(k2/m2)) 0];B=[0 0;1/m1 (b1*b2)/(m1*m2);0 -(b2/m2);(1/m1)+(1/m2) -(k2/m2)];C=[0 0 1 0]; D=[0 0];Aa = [[A,[0 0 0 0]\'];[C, 0]];Ba = [B;[0 0]];Ca = [C,0]; Da = D;K = [0 2.3e6 5e8 0 8e6];sys = ss(Aa-Ba(:,1)*K,Ba,Ca,Da);t = 0:0.01:5;r =0.1;initX1=0; initX1d=0;initX2=0; initX2d=0;[y,t,x]=lsim(sys*[0;1],r*ones(size(t)),t,[initX1;initX1d;initX2;initX2d;0]);save out.txt y"', $output); 
+      var_dump($output);
+    ?>
 </body>
 </html>
